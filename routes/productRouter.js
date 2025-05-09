423c40c7ff1f04691104af7c5e70860675a1706a
Mahith <mahithroxx@gmail.com>
2025-05-09 09:56:04 +0530

    Day 16🧨

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createProduct, getProducts, deleteProduct, updateProduct } from "../controllers/productController.js";
+import { createProduct, getProducts, deleteProduct, updateProduct, getProductById } from "../controllers/productController.js";
 
 const productRouter = express.Router();
 
 productRouter.post("/",createProduct)
 productRouter.get("/",getProducts)
+productRouter.get("/:id",getProductById)
 productRouter.delete("/:productId",deleteProduct)
 productRouter.put("/:productId",updateProduct)
 
